0d300fd0fc636de95aa0b573194c033b6d1b6fd9
Alfredo Medina <alfremedpal@gmail.com>
2022-09-03 14:10:10 -0500

    Implement basic query

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,19 @@ import db from '../db.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({
-        products: 'all'
-    })
+router.get('/', async (req, res) => {
+    let start = process.hrtime()
+    try {
+        const products = await db.any('SELECT * from products')
+        let stop = process.hrtime(start)
+        res.json({
+            data: products,
+            time: `${stop[0]} seconds and ${stop[1] / 1000000} milliseconds`
+        })
+    }
+    catch(err) {
+        console.log(err)
+    }
 })
 
 export default router
